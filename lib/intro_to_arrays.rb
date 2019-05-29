8cbfda95697b07158235552a672cca6adb18aab3
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-29 03:18:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,17 @@
+instantiate_new_array = []
+
+array_with_two_elements = [10, 20]
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+
